6a79dfe45f002844c669e8085ed01bd7dc6dfdfc
Andrew Graham <queglay@gmail.com>
2022-05-30 14:04:29 +0930

    Build with idempotence and erase existing

--- build.sh
+++ build.sh
@@ -2,8 +2,45 @@
 set -e
 set -x
 
+echo "Building AMI's for deployment..."
+
+function log {
+  local -r level="$1"
+  local -r message="$2"
+  local -r timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+  >&2 echo -e "${timestamp} [${level}] [$SCRIPT_NAME] ${message}"
+}
+
+function log_info {
+  local -r message="$1"
+  log "INFO" "$message"
+}
+
+function log_warn {
+  local -r message="$1"
+  log "WARN" "$message"
+}
+
+function log_error {
+  local -r message="$1"
+  log "ERROR" "$message"
+}
+
+function error_if_empty {
+  if [[ -z "$2" ]]; then
+    log_error "$1"
+  fi
+  return
+}
+
 EXECDIR="$(pwd)"
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # The directory of this script
+SOURCE=${BASH_SOURCE[0]}   # resolve the script dir even if a symlink is used to this script
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPTDIR=$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
 cd $SCRIPTDIR
 # source ../../../../update_vars.sh --sub-script --skip-find-amis
 # AMI TAGS
@@ -32,7 +69,42 @@ amazon-ebs.amazonlinux2-nicedcv-nvidia-ami,\
 amazon-ebs.centos7-ami,\
 amazon-ebs.base-openvpn-server-ami"
 
-# packer build "$@" $SCRIPTDIR/firehawk-base-ami.pkr.hcl
-packer build "$@" -only=$build_list $SCRIPTDIR/firehawk-base-ami.pkr.hcl
+missing_images_for_hash=$(aws ec2 describe-images --owners self --filters "Name=tag:commit_hash_short,Values=[$PKR_VAR_commit_hash_short]" --query "Images[*].{ImageId:ImageId,date:CreationDate,Name:Name,SnapshotId:BlockDeviceMappings[0].Ebs.SnapshotId,commit_hash_short:[Tags[?Key=='commit_hash_short']][0][0].Value,packer_source:[Tags[?Key=='packer_source']][0][0].Value}" \
+| jq -r '
+  .[].packer_source' \
+| jq --arg BUILDLIST "$build_list" --slurp --raw-input 'split("\n")[:-1] as $existing_names 
+| ($existing_names | unique) as $existing_names_set
+| ($BUILDLIST | split(",") | unique) as $intended_names_set
+| $intended_names_set - $existing_names_set
+')
+
+count_missing_images_for_hash=$(jq -n --argjson data "$missing_images_for_hash" '$data | length')
+
+if [[ "$count_missing_images_for_hash" -eq 0 ]]; then
+  echo "All images have already been built for this hash and build list."
+  echo
+  echo "To force a build, ensure at least one image from the build list is missing.  The builder will erase all images for the commit hash and rebuild."
+
+  cd $EXECDIR
+  set +e
+  exit 0
+fi
+
+echo "The following images have not yet been built:"
+echo "$missing_images_for_hash"
+echo "Packer will erase all images for this commit hash and rebuild all images"
+
+$SCRIPTDIR/delete-all-old-amis.sh --commit-hash-short-list $PKR_VAR_commit_hash_short
+
+# Validate
+packer validate "$@" -var "ca_public_key_path=$HOME/.ssh/tls/ca.crt.pem" \
+  -only=$build_list \
+  $SCRIPTDIR/firehawk-base-ami.pkr.hcl
+
+# Build
+packer build "$@" -var "ca_public_key_path=$HOME/.ssh/tls/ca.crt.pem" \
+  -only=$build_list \
+  $SCRIPTDIR/firehawk-base-ami.pkr.hcl
 
 cd $EXECDIR
+set +e
